0a16786ef8dc049200fa9e37e9f1fe17ba4b3641
Agustin Perez <agustinnicolas806@gmail.com>
2023-09-30 11:46:51 -0300

    Move vit svg to assets to avoid warning

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css';
-import reactLogo from '@assets/react.svg';
 
-import viteLogo from '../../../../vite.svg';
+import reactLogo from '@assets/react.svg';
+import viteLogo from '@assets/vite.svg';
 
 function App() {
   return (
