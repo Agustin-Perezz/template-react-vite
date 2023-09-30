b01fc054efe099eeaf7447f1c179a90508c739a2
Agustin Perez <agustinnicolas806@gmail.com>
2023-09-30 11:40:07 -0300

    Update eslint config

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import reactLogo from '@assets/react.svg';
-import viteLogo from '../../../../vite.svg';
 import './App.css';
+import reactLogo from '@assets/react.svg';
+
+import viteLogo from '../../../../vite.svg';
 
 function App() {
   return (
